269d6efc871a3ffd0e177dec4162559f662c2741
Morteza Jamshidi <devmor.j@gmail.com>
2022-05-07 21:12:44 +0430

    Write tests for quote card component

---
+++ src/components/__tests__/QuoteCard.spec.js
@@ -0,0 +1,34 @@
+import {describe, it, expect,beforeEach, afterEach} from 'vitest';
+import {shallowMount, enableAutoUnmount} from "@vue/test-utils";
+
+import QuoteCard from '../QuoteCard.vue';
+
+describe('QuoteCard', () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = shallowMount(QuoteCard);
+  });
+  
+  enableAutoUnmount(afterEach);
+
+  it('has get-advice button', () => {
+    expect(wrapper.get('button.get-advice').exists()).toBe(true);
+  });
+
+  it('has quote element', () => {
+    expect(wrapper.get('q').exists()).toBe(true);
+  });
+
+  it('has default quote advice', () => {  
+    expect(wrapper.get('q').text()).toBeTruthy();
+  });
+
+  it('does not show error messages on startup', () => {
+    expect(wrapper.find('.error').exists()).toBe(false);
+  });
+
+  it('click works on the get-advice button', async () => {
+    await wrapper.get('button.get-advice').trigger('click');
+  });
+})
